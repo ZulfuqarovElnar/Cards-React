04ff810dcb3e830a7f8b0e5fbad15c657f6e9df9
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2023-11-26 15:00:37 +0400

    Navbar add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import "./assets/css/tailwind.css"
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
