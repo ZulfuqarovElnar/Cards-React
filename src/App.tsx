14144e5f553429b38d468794a191c9591a52bf6f
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2023-11-27 21:27:53 +0400

    Form add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/navbar';
-import "./assets/css/tailwind.css"
+import './assets/css/tailwind.css';
+import Form from './components/form';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Form />
+      </div>
+    </Router>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  console.error('Hata: "root" ID\'li bir öğe bulunamadı.');
+} else {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
